1ea39a6865e22d5613688b61958807aee804f6a5
Pp-4 <39858427+Pp-4@users.noreply.github.com>
2026-04-21 16:25:36 +0200

    More SI units

--- src/Utility/Parsing.cs
+++ src/Utility/Parsing.cs
@@ -12,16 +12,17 @@ public static class Parsing
             //area, volume
             "mm2" or "mm3" or "cm2" or "cm3" or "dm2" or "dm3" or "m2" or "m3" or "km2" or "km3" or "l" => siUnit,
             //mass
-            "mg" or "g" or "dkg" or "kg" => siUnit,
+            "mcg" or "mg" or "g" or "dkg" or "kg" => siUnit,
             //time
-            "h" or "min" or "s" => siUnit,
+            "h" or "min" or "s" or "ms" or "µs" => siUnit,
             //various
             "lm" or "lx" or "lux" or "mol" => siUnit,
             "m/s" or "m/s2" or "kg/m2" or "kg/m3" or "m2/kg" or "m3/kg" => siUnit,
             //frequency
             "hz" or "khz" or "mhz" or "ghz" or "thz" => Capitalize(siUnit, siUnit.Length - 1),
             "dba" or "db(a)" => "dBA",
-            "kw" or  "mw" or "gw" or "tw" or "ka" or "ma" or "ga" or "kj" or "mj" or "gj" or "tj" => Socialise(siUnit),
+            //energy
+            "kw" or "mw" or "gw" or "tw" or "ka" or "ma" or "ga" or "kj" or "mj" or "gj" or "tj" => Socialise(siUnit),
             _ => siUnit.ToUpper(),
         };
     }
