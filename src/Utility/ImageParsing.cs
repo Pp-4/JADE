0e000266e3151b888e882cc6dddc63eb40ca817a
Pp-4 <39858427+Pp-4@users.noreply.github.com>
2026-05-18 17:15:51 +0200

    add  conversion of webp to png

---
+++ src/Utility/ImageParsing.cs
@@ -0,0 +1,22 @@
+using System;
+using SkiaSharp;
+
+namespace JADE.Utility;
+
+
+public static class ImageParsing
+{
+    public static byte[] ConvertWebpToPng(byte[] imageData)
+    {
+
+        using var stream = new SKMemoryStream(imageData);
+
+        using var rawData = SKBitmap.Decode(stream);
+        if (rawData is not null)
+        {
+            var image = SKImage.FromBitmap(rawData);
+            return image.Encode(SKEncodedImageFormat.Png, 100).ToArray();
+        }
+        throw new NullReferenceException("Ivalid Image!");
+    }
+}
